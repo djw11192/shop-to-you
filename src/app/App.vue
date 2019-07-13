79cc75b70ced7bb48030228aac06e76063b9b004
David Weber <davidjwe33@gmail.com>
2019-07-13 15:00:03 -0700

    desktop header markup and style

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,29 +1,34 @@
 <template>
-  <div id="app">
-    <Home />
+	<div id="app">
+		<Header />
+		<main class="main">
+			<Home />
+		</main>
 		<Footer />
-  </div>
+	</div>
 </template>
 
 <script>
-import axios from 'axios';
-import Home from '../views/home/Home.vue'
+// import axios from 'axios';
+import Home from '../views/home/Home.vue';
 import Footer from '@/components/layout/footer/Footer.vue';
+import Header from '@/components/layout/header/Header.vue';
 
 export default {
-  name: 'app',
-  components: {
-    Home,
-		Footer
-  },
+	name: 'app',
+	components: {
+		Home,
+		Footer,
+		Header
+	},
 	created() {
 		// axios.get('http://localhost:3000/')
-  	// 	.then(response => {
-    //   	console.log(response);
-    // 	})
-    // 	.catch(e => {
-    //   	console.log(e);
-    // 	})
+		// 	.then(response => {
+		//   	console.log(response);
+		// 	})
+		// 	.catch(e => {
+		//   	console.log(e);
+		// 	})
 	}
 }
 </script>
---
+++ src/components/layout/header/Header.vue
@@ -0,0 +1,29 @@
+<template>
+	<header class="hdr">
+		<div class="container">
+			<div class="hdr__inner">
+				<div class="hdr__logo-wrap">
+					<img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/-180/-2141186180_5a4ec33f-637e-44b1-8909-ef6b307cc69b.png?cb=1563050008" />
+				</div>
+				<div class="hdr__utils">
+					<a href="/" class="btn btn--secondary hdr__util-btn">
+						Sign In
+					</a>
+					<a href="/" class="btn btn--primary hdr__util-btn">
+						Shop Now
+					</a>
+				</div>
+			</div>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	name: 'Header',
+}
+</script>
+
+<style lang="scss">
+@import 'Header.scss';
+</style>
